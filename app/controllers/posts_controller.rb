1c34548f813198ffd00b724ea5aaf0ac11b65742
Kazama <narusai97@gmail.com>
2016-07-03 17:30:03 +0900

    ログイン画面追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only:[:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
   def index
     @q = Post.order(created_at: :desc).ransack(params[:q])
     @posts = @q.result.page(params[:page]).per(2)
